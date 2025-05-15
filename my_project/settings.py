c171bdf121a393f50c7b1824094f0401f5cdd998
sims-bit <xfiqoe46@students.codeinstitute.net>
2025-05-15 10:54:35 +0100

    Create Procfile

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_cw&mx7%feoh5vj8sznj!r*uzz@7h&)8-(ampjt+7+)%(fdd+u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
