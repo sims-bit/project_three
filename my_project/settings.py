941f729b4ee8dd53c48dc77476bd0be083184dc6
sims-bit <xfiqoe46@students.codeinstitute.net>
2025-05-15 12:57:11 +0100

    Add post model

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_cw&mx7%feoh5vj8sznj!r*uzz@7h&)8-(ampjt+7+)%(fdd+u'
+# SECRET_KEY = 'django-insecure-_cw&mx7%feoh5vj8sznj!r*uzz@7h&)8-(ampjt+7+)%(fdd+u'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -88,6 +89,11 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
