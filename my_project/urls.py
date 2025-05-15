b87ef0764d08fe5537280ede361b6e9a7a993ed4
sims-bit <xfiqoe46@students.codeinstitute.net>
2025-05-15 15:11:37 +0100

    Create generic post view

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from my_site import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("my_site.urls"), name="my_site-urls"),
     path('admin/', admin.site.urls),
 ]
--- my_site/views.py
+++ my_site/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-def index(request):
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
